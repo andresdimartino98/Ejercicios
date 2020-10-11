5d65090f982fb24cd40820727d660bc73771e265
andresdimartino <arte.dimartino@gmail.com>
2020-10-11 19:58:55 -0300

    agrego ejercicio 1 para probar , luego los demas

--- src/Principal.java
+++ src/Principal.java
@@ -1,9 +1,42 @@
+import java.util.Scanner;
 
 public class Principal {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 //aca voy a poner mis ejercicios para subir a git
+		
+		Scanner sc = new Scanner (System.in);
+		int numeroA;
+		int numeroB;
+		int resultado;
+
+		try
+		{
+		System.out.println("ingrese numero entero");
+		numeroA = sc.nextInt();
+
+		System.out.println("ingrese numero entero");
+		numeroB = sc.nextInt();
+
+		resultado= numeroA + numeroB;
+
+		System.out.println("la suma es " + resultado);
+
+
+		}
+		catch (Exception ex)
+		{
+			System.out.println("ingreso un numero inesperado ");
+		}
+			}
+		
+		
+		
+		
+		
+		
+		
 	}
 
-}
+
