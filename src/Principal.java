14e8b03fa7087e7ee0f4aff7750b6b64f1269f8c
andresdimartino <arte.dimartino@gmail.com>
2020-10-12 20:41:00 -0300

    subo los tres que funcionan , aunque esta mal escrito una abajo del otro porque no me acuerdo como era para poner varios en uno mismo

--- src/Principal.java
+++ src/Principal.java
@@ -1,3 +1,5 @@
+
+//ejercicio 1
 import java.util.Scanner;
 
 public class Principal {
@@ -30,13 +32,107 @@ public class Principal {
 			System.out.println("ingreso un numero inesperado ");
 		}
 			}
+	
+}
+		
+
+
+//ejercicio dos
+
+public class ejerciciodos {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner sc = new Scanner (System.in);
+		
+		int numero1;
+		int numero2;
+		int resultado;
 		
 		
+		System.out.println("ingrese numero entero");
+		numero1 = sc.nextInt();
 		
+		System.out.println("ingrese otro numero entero");
+		numero2 = sc.nextInt();
 		
+		if ( numero1 < numero2 ) {
+	  System.out.println("el nuemero mayor es:" + numero2);
+		}
+		else{
 		
+			System.out.println("el nuemero mayor es:" + numero1);
 		
-		
+			}
+	
+		if (numero2 == numero1){
+			System.out.println("los numeros son iguales:" + numero2);
+		}
 	}
+
+}
+		
+		
+		
+		//ejercicio seis
+
+import java.util.Scanner;
+
+public class ejercicioseis2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		{
+		Scanner sc = new Scanner (System.in);
+		short mes;
+		
+		
+		System.out.println("ingrese un mes ");
+		mes = (short) sc.nextInt();
+		
+		
+		switch(mes) {
+		  case 1:
+		  case 3:
+		  case 5:
+		  case 7:
+		  case 8:
+		  case 10:
+		  case 12:
+			  
+			  System.out.println("el mes tiene 31 días ");
+		    break;
+		  
+		  case 2:
+			  System.out.println("el mes tiene 28 días ");
+			 
+		    break;
+		 
+		  case 4:
+		  case 6:
+		  case 9:
+		  case 11:
+			  
+			  System.out.println("el mes tiene 30 días ");
+		 
+			  
+			  
+			    break; 
+		    
+		    
+		  default:
+			  
+			  System.out.println("el número ingresado no corresponde a un mes, ingrese numeros del 1 al 12");
+		}
+
+	}
+
+}
+}
+		
+		
+
+
 
 
